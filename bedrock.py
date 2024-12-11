ce0ab15043b00f69bff6fee62a7880be4bb32313
Andrei Filimonov <afilimonov@gmail.com>
2024-12-11 22:19:31 +0000

    Refactoring

--- bedrock.py
+++ bedrock.py
@@ -10,6 +10,7 @@ class BedrockModel(ABC):
     
     def __init__(self, model, temperature=0, max_tokens=4096, top_p=0.9, top_k=256):
         self.model = model
+        self.temperature = temperature
         self.max_tokens = max_tokens
         self.top_p = top_p
         self.top_k = top_k
@@ -22,14 +23,14 @@ class BedrockModel(ABC):
     def get_content(response):
         pass
     
-    def call(self, instrucitons, messages):
+    def invoke(self, instructions, messages):
         brt = boto3.client(service_name='bedrock-runtime')
     
         body = self.get_body(instructions, messages)
         accept = 'application/json'
         contentType = 'application/json'
 
-        response = brt.invoke_model(body=body, modelId=model, accept=accept, contentType=contentType)
+        response = brt.invoke_model(body=body, modelId=self.model, accept=accept, contentType=contentType)
 
         return self.get_content(json.loads(response.get('body').read())) 
 
@@ -41,11 +42,11 @@ class Jamba(BedrockModel):
     def get_body(self, instructions, messages):
         system = [{
             "role": "system",
-            "content": innstrucitons
+            "content": instructions
         }]
         
         return json.dumps({
-        "messages": system + message,
+        "messages": system + messages,
         "max_tokens": self.max_tokens,
         "temperature": self.temperature,
         "top_p": self.top_p 
@@ -53,5 +54,27 @@ class Jamba(BedrockModel):
 
     
     #return response_body.get('content')[0]['text']    
-    def get_content(response):
+    def get_content(self, response):
         return response.get('choices')[0].get('message').get('content') 
+
+class Claude(BedrockModel):
+    sonnet35 = 'us.anthropic.claude-3-5-sonnet-20241022-v2:0'
+    sonnet35_arn = 'arn:aws:bedrock:us-east-1:827075926781:inference-profile/us.anthropic.claude-3-5-sonnet-20241022-v2:0'
+    haiku35 = 'us.anthropic.claude-3-5-haiku-20241022-v1:0'
+    haiku35_arn = 'arn:aws:bedrock:us-east-1:827075926781:inference-profile/us.anthropic.claude-3-5-haiku-20241022-v1:0'
+    sonnet3 = 'anthropic.claude-3-sonnet-20240229-v1:0'
+    haiku3 = 'anthropic.claude-3-haiku-20240307-v1:0'
+    
+    def get_body(self, instructions, messages):
+        return json.dumps({
+        "anthropic_version": "bedrock-2023-05-31",
+        "system": instructions,
+        "messages": messages,
+        "max_tokens": self.max_tokens,
+        "temperature": self.temperature,
+        "top_p": self.top_p,
+        "top_k": self.top_k
+        })
+    
+    def get_content(self, response):
+        return response.get('content')[0]['text'] 
